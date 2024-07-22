bb377aa5178206b1f2212fb6fbac9e67cc534040
Jethro Beekman <jethro@jbeekman.nl>
2024-07-22 20:57:21 +0200

    Show timer before contest start

--- scripts/pageController.js
+++ scripts/pageController.js
@@ -18,12 +18,25 @@
 const timingsDefault = { startTime: null, endTime: 0 };
 
 function computeState(timings, currentTime) {
-    if (timings.startTime === null || currentTime < timings.startTime) {
+    if (timings.startTime === null) {
         return { state: "logo", end: false };
     } else if (currentTime >= timings.endTime + blinkSeconds) {
         return { state: "logo", end: true };
+    } else if (currentTime < timings.startTime) {
+        let remainingTime = timings.startTime - currentTime;
+        [remainingTime, s] = divMod(remainingTime, 60);
+        s = Math.round(s);
+        const [h, m] = divMod(remainingTime, 60);
+        if (h > 0 || m >= quarterMinutesDisplay) {
+            return { state: "time", h, m, blink: false, before_start: true };
+        } else if (m >= secondsDisplay) {
+            return { state: "time", h, m, s: Math.floor(s / 15) * 15, blink: false, before_start: true };
+        } else {
+            return { state: "time", h, m, s, blink: false, before_start: true };
+        }
     } else {
         let remainingTime = timings.endTime - currentTime;
+        const justStarted = (currentTime - timings.startTime) < 1;
         let blink = ((Math.round(remainingTime) % 2) === 0);
         if (remainingTime < 0) {
             remainingTime = 0;
@@ -32,9 +45,9 @@ function computeState(timings, currentTime) {
         s = Math.round(s);
         const [h, m] = divMod(remainingTime, 60);
         if (h > 0 || m >= quarterMinutesDisplay) {
-            return { state: "time", h, m, blink: false };
+            return { state: "time", h, m, blink: justStarted };
         } else if (m >= secondsDisplay) {
-            return { state: "time", h, m, s: Math.floor(s / 15) * 15, blink: false };
+            return { state: "time", h, m, s: Math.floor(s / 15) * 15, blink: justStarted };
         } else {
             if (s >= blinkSeconds) {
                 blink = false;
@@ -107,8 +120,9 @@ const pageController = {
         switch (state.state) {
             case "logo":
                 this.done = state.end;
-                document.querySelector(".logo").classList.remove("hidden");
+                document.querySelector(".logo").classList.remove("withtimer");
                 document.querySelector(".timer").classList.add("hidden");
+                document.querySelector(".banner").classList.add("hidden");
                  break;
             case "time":
                 const pad = (i) => i.toString().padStart(2, "0");
@@ -120,8 +134,9 @@ const pageController = {
                 } else {
                     document.querySelector(".timer .seconds").classList.add("hidden");
                 }
+                document.querySelector(".banner").classList.toggle("hidden", !state.before_start);
                 document.querySelector(".timer").classList.toggle("blink", state.blink);
-                document.querySelector(".logo").classList.add("hidden");
+                document.querySelector(".logo").classList.add("withtimer");
                 document.querySelector(".timer").classList.remove("hidden");
                  break;
         }
